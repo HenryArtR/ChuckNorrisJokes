41b990d0ed016580c518dcad032930723423b807
Herian369 <henryarturorivera@gmail.com>
2021-11-27 16:31:25 +0100

    Sprint 5 terminado

--- dist/index.js
+++ dist/index.js
@@ -1,87 +1,101 @@
-"use strict";
+'use strict';
+
 // urls
 const url1 = 'https://icanhazdadjoke.com/';
-const url2 = 'https://api.openweathermap.org/data/2.5/weather?q=barcelona,es&appid=21c127bb06111bdb9012ed3611eb5b2b';
+const url2 =
+  'https://api.openweathermap.org/data/2.5/weather?q=barcelona,es&appid=21c127bb06111bdb9012ed3611eb5b2b';
 const url3 = 'https://api.chucknorris.io/jokes/random';
 // html getters
 const info = document.getElementById('info');
 const p = document.getElementById('joke');
+const fondo = document.getElementById('fondo');
 // objects
 const option = {
-    headers: {
-        Accept: 'application/json',
-    },
+  headers: {
+    Accept: 'application/json',
+  },
 };
 // Arrays
-const random = [];
-const acudits = [];
+let jokesArr = [];
+let acudits = [];
 const reportJokes = [];
+// random
+let item = 0;
+function random() {
+  item = Math.floor(Math.random() * 2);
+  return jokesArr[item];
+}
+let num = 0;
+function randomFondo() {
+  num = Math.ceil(Math.random() * 5);
+  fondo.classList.add(`fondo${num}`);
+}
+randomFondo();
 // joke
 function promise() {
-    fetch(url1, option)
-        .then((response) => response.json())
-        .then((jokes) => random.push(jokes.joke))
-        .catch((err) => alert(err));
+  fetch(url1, option)
+    .then((response) => response.json())
+    .then((jokes) => jokesArr.push(jokes.joke))
+    .catch((err) => err);
 }
+promise();
+// chuckNorris
+function jokeNorris() {
+  fetch(url3)
+    .then((response) => response.json())
+    .then((joke) => {
+      jokesArr.push(joke.value);
+      p.textContent = jokesArr[item];
+    })
+    .catch((err) => err);
+}
+jokeNorris();
 // weather
 let country = '';
 let nom = '';
 let icon = '';
 let grados = '';
 function weather() {
-    fetch(url2)
-        .then((response) => response.json())
-        .then((data) => {
-        grados = (data.main.temp - 273.15).toFixed(1);
-        nom = data.name;
-        icon = data.weather[0].icon;
-        country = data.sys.country;
-        info.innerHTML = `
+  fetch(url2)
+    .then((response) => response.json())
+    .then((data) => {
+      grados = (data.main.temp - 273.15).toFixed();
+      nom = data.name;
+      icon = data.weather[0].icon;
+      country = data.sys.country;
+      info.innerHTML = `
       <img src="http://openweathermap.org/img/wn/${icon}@2x.png" width="50" height="50">
       <span>${grados}ºC ${nom}, ${country}</span>
       `;
     })
-        .catch((err) => alert(err));
+    .catch((err) => err);
 }
-window.onload = weather;
-// chuckNorris
-function jokeNorris() {
-    fetch(url3)
-        .then((response) => response.json())
-        .then((joke) => random.push(joke.value))
-        .catch((err) => alert(err));
-}
+weather();
 // functions
-function getRandomJoke(rdm) {
-    const randomItem = Math.floor(Math.random() * rdm.length);
-    item = rdm[randomItem];
-    return item;
-}
-window.addEventListener('load', getRandomJoke(random));
 function rateJoke(scoreJoke) {
-    const d = new Date();
-    d.setHours(d.getHours() + 1);
-    const textDate = d.toISOString();
-    const rate = {
-        joke: getRandomJoke(random),
-        score: scoreJoke,
-        date: textDate,
-    };
-    acudits.push(rate);
+  const d = new Date();
+  d.setHours(d.getHours() + 1);
+  const textDate = d.toISOString();
+  const rate = {
+    joke: jokesArr[item],
+    score: scoreJoke,
+    date: textDate,
+  };
+  acudits.push(rate);
 }
 function finalRate() {
-    let final = acudits.pop();
-    reportJokes.push(final);
-    console.log(reportJokes);
+  const final = acudits.pop();
+  reportJokes.push(final);
+  acudits = [];
+  console.log(reportJokes);
 }
 function nextJoke() {
-    promise();
-    jokeNorris();
-    finalRate();
-    if (p.textContent == null) {
-        return p.textContent = getRandomJoke(random);
-    }
-    else {
-    }
+  promise();
+  jokeNorris();
+  finalRate();
+  fondo.classList.remove(`fondo${num}`);
+  randomFondo();
+  p.textContent = random();
+  jokesArr = [];
 }
-//# sourceMappingURL=index.js.map
+// # sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const url3 = 'https://api.chucknorris.io/jokes/random';
 // html getters
 const info = document.getElementById('info');
 const p = document.getElementById('joke');
+const fondo = document.getElementById('fondo');
 // objects
 const option: object = {
   headers: {
@@ -19,27 +20,53 @@ interface RateJoke {
   date: string;
 }
 // Arrays
-const random: string[] = [];
-const acudits: object[] = [];
+let jokesArr: string[] = [];
+let acudits: object[] = [];
 const reportJokes: object[] = [];
-// joke
 
-function promise() {
+// random
+let item: number = 0;
+function random(): string {
+  item = Math.floor(Math.random() * 2);
+  return jokesArr[item];
+}
+let num: number = 0;
+function randomFondo() {
+  num = Math.ceil(Math.random() * 5);
+  fondo!.classList.add(`fondo${num}`);
+}
+randomFondo();
+
+// joke
+function promise(): any {
   fetch(url1, option)
     .then((response) => response.json())
-    .then((jokes) => random.push(jokes.joke))
-    .catch((err) => alert(err));
+    .then((jokes) => jokesArr.push(jokes.joke))
+    .catch((err) => err);
 }
+promise();
+// chuckNorris
+function jokeNorris(): any {
+  fetch(url3)
+    .then((response) => response.json())
+    .then((joke) => {
+      jokesArr.push(joke.value);
+      p!.textContent = jokesArr[item];
+    })
+    .catch((err) => err);
+}
+jokeNorris();
 // weather
 let country: string = '';
 let nom: string = '';
 let icon: string = '';
-let grados: any = '';
-function weather() {
+let grados: string = '';
+
+function weather(): any {
   fetch(url2)
     .then((response) => response.json())
     .then((data) => {
-      grados = (data.main.temp - 273.15).toFixed(1);
+      grados = (data.main.temp - 273.15).toFixed();
       nom = data.name;
       icon = data.weather[0].icon;
       country = data.sys.country;
@@ -48,52 +75,36 @@ function weather() {
       <span>${grados}ºC ${nom}, ${country}</span>
       `;
     })
-    .catch((err) => alert(err));
+    .catch((err) => err);
 }
-window.onload = weather;
-// chuckNorris
-function jokeNorris() {
-  fetch(url3)
-    .then((response) => response.json())
-    .then((joke) => random.push(joke.value))
-    .catch((err) => alert(err));
-}
-
+weather();
 
 // functions
-
-function getRandomJoke(rdm: any) {
-  const randomItem = Math.floor(Math.random() * rdm.length);
-  item = rdm[randomItem];
-  return item;
-}
-window.addEventListener('load', getRandomJoke(random))
 function rateJoke(scoreJoke: number) {
   const d = new Date();
   d.setHours(d.getHours() + 1);
   const textDate = d.toISOString();
   const rate: RateJoke = {
-    joke: getRandomJoke(random),
+    joke: jokesArr[item],
     score: scoreJoke,
     date: textDate,
   };
-
   acudits.push(rate);
 }
-function finalRate(){
-  let final = acudits.pop()
-  reportJokes.push(final)
-  console.log(reportJokes)
+
+function finalRate(): any {
+  const final: any = acudits.pop();
+  reportJokes.push(final);
+  acudits = [];
+  console.log(reportJokes);
 }
 
-function nextJoke(): string {
+function nextJoke(): any {
   promise();
   jokeNorris();
-  finalRate()
-  if(p!.textContent == null){
-    return p!.textContent = getRandomJoke(random)
-  }else{
-    
-  }
-  
+  finalRate();
+  fondo!.classList.remove(`fondo${num}`);
+  randomFondo();
+  p!.textContent = random();
+  jokesArr = [];
 }
